ff2b802b37610421028432c13b3f76e2ddf8aae4
Aadarsh <roxkaadarsh@gmail.com>
2018-06-20 15:41:39 +0530

    Create Kenzobuilder.sh

---
+++ Kenzobuilder.sh
@@ -0,0 +1,10 @@
+--------------------------------
+
+mkdir -p .repo/local_manifests
+
+wget https://raw.githubusercontent.com/aadiiii/roms/master/rom.xml -O .repo/local_manifests/rom.xml
+
+repo sync
+
+-------------------------------
+
